e1857ebd1002a1cfc3ebacb854fa9d3c1097b3c7
TiveCS <ahmadfathanhanif@gmail.com>
2023-05-05 20:02:55 +0700

    initial commit
    Signed-off-by: TiveCS <ahmadfathanhanif@gmail.com>

---
+++ src/main/java/io/github/tivecs/quailmail/QuailMail.java
@@ -0,0 +1,17 @@
+package io.github.tivecs.quailmail;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuailMail extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
